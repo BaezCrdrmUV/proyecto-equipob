7c2ce37ae01116181088991e0aed0d4d6a61171f
Frews2 <parkersjames87@gmail.com>
2021-06-17 00:14:27 -0500

    Creación de scripts para las bases de datos de mongo

---
+++ PrivateLibrary.js
@@ -0,0 +1,15 @@
+db.Genre.insert([
+	{ "Id": "1",
+	"Name": "Jazz"},
+	{ "Id": "2",
+	"Name": "Rock"},
+	{ "Id": "3",
+	"Name": "Metal"},
+	{"Id": "4",
+	"Name": "Pop"}])
+
+db.SongStatus.insert([
+	{ "Id": "1",
+	"Name": "Disponible"},
+	{"Id": "2",
+	"Name": "Solicitado"}])
---
+++ PublicLibrary.js
@@ -0,0 +1,17 @@
+db.Genre.insert([
+	{ "Id": "1",
+	"Name": "Jazz"},
+	{ "Id": "2",
+	"Name": "Rock"},
+	{ "Id": "3",
+	"Name": "Metal"},
+	{"Id": "4",
+	"Name": "Pop"}])
+
+db.SongStatus.insert([
+	{ "Id": "1",
+	"Name": "Aprobado"},
+	{ "Id": "2",
+	"Name": "Rechazado"},
+	{"Id": "3",
+	"Name": "Pendiente"}])
