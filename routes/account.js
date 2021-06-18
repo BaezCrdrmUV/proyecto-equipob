76491783bcb338724b8dea7b2f720d609be93892
Pklove3459 <delmanclen@hotmail.com>
2021-06-18 16:09:22 -0500

    Mobile Support

--- routes/account.js
+++ routes/account.js
@@ -25,6 +25,30 @@ router.post("/login", async (req, res) => {
     })
 })
 
+router.post("/loginMobile", async (req, res) => {
+    let password = req.body.PasswordString;
+    req.body.PasswordString = hashText(password);
+    microservicioAccount.Login(req.body)
+    .then(values => {
+        res.send(values);
+    })
+    .catch(error => {
+        res.send("Account/loginMovile", error);
+    })
+})
+
+    router.post("/registerNewAccountMobile", async(req, res) =>{
+        let passwordNew = req.body.Passwords[0].PasswordString;
+        req.body.Passwords[0].PasswordString = hashText(passwordNew);
+        microservicioAccount.RegisterNewAccount(req.body)
+        .then(values => {
+            res.send(values);
+        })
+        .catch(error => {
+            res.send("Account/register",error);
+        })
+    })
+
 router.post("/registerNewAccount", async(req, res) =>{
     microservicioAccount.RegisterNewAccount(req.body)
     .then(values => {
@@ -43,6 +67,8 @@ router.put("/updateAccount", async(req, res) =>{
     .catch(error => {
         res.send("Account/updateAccount",error);
     })
+
+    
 })
 
 router.put("/banAccount", async(req, res) =>{
@@ -66,5 +92,22 @@ router.put("/updatePassword", async(req, res) =>{
         res.send("Password/updatePassword",error);
     })
 })
+
+
+
+function hashText(text) 
+{
+    if (typeof text != 'string') {
+        throw TypeError('El argumento debe ser una cadena de caracteres.');
+    }
+
+    if (!text.length) {
+        return null;
+    }
+
+    let caracteres = text.split('');
+
+    return caracteres.reduce((h, c) => (h = c.charCodeAt(0) + (h << 6) + (h << 16) - h), 0);
+}
 
 export default router;
